c3b20f53aa47467c4a089e8282c35fe7e81a5437
Sergei Iniakin <inyakin97@gmail.com>
2022-05-13 08:23:52 +0300

    add nodes to test tree

--- post_order_dfs.py
+++ post_order_dfs.py
@@ -34,8 +34,9 @@ tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
-
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
 
 # Test
-# Should be [4, 5, 2, 3, 1]
+# Should be [4, 5, 2, 6, 7, 3, 1]
 print(tree.post_order_dfs())
